7d911e5b054a9168514f00a1d84ef2e2698b0655
leenstitch <leen.samaneh@gmail.com>
2026-07-27 10:45:11 +0300

    Implement LINQ grouping joining and SelectMany exercises for week2 - day2

---
+++ Week2/Day1-Day2/Models/Order.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Models
+{
+    // Order class represents a customer's order in the library system
+    public class Order
+    {
+        public Guid Id { get; private set; }
+
+
+        public Customer Customer { get; private set; }
+
+
+        public List<OrderItem> Items { get; private set; }= new();
+
+        public decimal Total => CalculateTotal();
+        // Constructor for Order class
+        public Order(Customer customer)
+        {
+            Id = Guid.NewGuid();
+
+            Customer = customer;
+
+            customer.Orders.Add(this);
+        }
+
+
+        //============= METHODS ==========
+
+        // Method to calculate the total price of the order
+        public decimal CalculateTotal()
+        {
+            return Items.Sum(x => x.SubTotal);
+        }
+
+
+        // Method to add an item to the order
+        public void AddItem(Book book, int quantity)
+        {
+            OrderItem item = new OrderItem(
+                book,
+                quantity
+            );
+
+            Items.Add(item);
+        }
+    }
+}
---
+++ Week2/Day1-Day2/Repositories/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibrarySystem.Interfaces.RepositoryInterfaces;
+
+namespace LibrarySystem.Repositories
+{
+    // Generic repository class that implements IRepository<T> interface
+    public class Repository<T> : IRepository<T>
+    where T : class // Generic type constraint to ensure T is a reference type
+    {
+
+        protected readonly List<T> Items = new();// Internal list to store items of type T
+
+
+        // Method to add an item of type T to the repository
+        public void Add(T item)
+        {
+            Items.Add(item);
+        }
+
+
+        // Method to remove an item of type T from the repository
+        public IReadOnlyList<T> GetAll()
+        {
+            return Items.AsReadOnly();
+        }
+
+
+        // Method to find an item of type T by its unique identifier (Guid)
+        public T? Find(Guid id)
+        {
+            return Items.FirstOrDefault();
+        }
+
+    }
+}
