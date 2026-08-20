c0d2bce32a6655c208ae6e9dc5493e3ab1c3dfa0
leenstitch <leen.samaneh@gmail.com>
2026-08-20 16:05:29 +0300

    Complete Week 5 testing and project updates

--- Cardiac Patient Monitoring System/Cardiac Patient Monitoring System/Interfaces/IJwtService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Cardiac_Patient_Monitoring_System.Models;
-
-namespace Cardiac_Patient_Monitoring_System.Interfaces
-{
-    public interface IJwtService
-    {
-        Task<string> CreateToken(ApplicationUser user);
-    }
-}
